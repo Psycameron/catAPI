e7e8d9c4ef93771b134e00d407a12b3045f93958
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-08-22 12:18:32 +0300

    feat: add base HTML for Header, Navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Jost } from "next/font/google";
+import Navbar from "@/components/Navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({ subsets: ["latin"] });
 
 export const metadata = {
   title: "PestPaw app",
@@ -11,7 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jost.className}>
+        <div>
+          <Header />
+          <Navbar />
+        </div>
+        <main className="mainBg">{children}</main>
+      </body>
     </html>
   );
 }
