c28fe40088faf6ac807cc22b7debf826176fe0b4
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-08-24 21:56:34 +0300

    feat(breeds): add dynamic routing

--- src/components/GridPattern/GridPattern.jsx
+++ src/components/GridPattern/GridPattern.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./GridPattern.module.css";
+import Link from "next/link";
 
 export default function GridPattern({ cats }) {
   // console.log(`🚀 ~ GridPattern ~ cats:`, cats);
@@ -9,15 +10,18 @@ export default function GridPattern({ cats }) {
   }
 
   return (
-    <div className={styles.parent}>
+    <ul className={styles.parent}>
       {cats.map(({ url, id, breeds }) => {
+        console.log(`🚀 ~ {cats.map ~ id:`, id);
         return (
-          <div className={styles.item} key={id}>
-            <Image src={url} alt={breeds[0].name} width={320} height={320} />
-          </div>
+          <li className={styles.item} key={id}>
+            <Link href={`/breeds/${id}`}>
+              <Image src={url} alt={breeds[0].name} width={320} height={320} />
+            </Link>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 }
 
