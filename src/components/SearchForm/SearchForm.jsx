c38a29ce5c6585898b133e62728138f232bbb405
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-08-28 02:56:59 +0300

    styles(): add styles for SearchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,18 @@
 "use client";
 
+import styles from "./SearchForm.module.css";
 import SearchIcon from "public/images/svg/search.svg";
 
 export default function SearchForm() {
   return (
-    <div>
-      <input type="text" placeholder="Search for breeds by name" />
-      <button type="submit">
-        <SearchIcon />
+    <div className={styles.wrapper}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Search for breeds by name"
+      />
+      <button className={styles.button} type="submit">
+        <SearchIcon className={styles.icon} />
       </button>
     </div>
   );
