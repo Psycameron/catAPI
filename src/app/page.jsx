f6d10e03f35021c694df942a55497248ece0fd04
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-08-27 23:43:03 +0300

    styles: add content for Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Paper from "@/components/Paper/Paper";
+
+import girlAndPet from "/public/images/girl-and-pet.png";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Paper color="custom" />
+      <Paper color="custom">
+        <Image
+          className={styles.image}
+          src={girlAndPet}
+          alt="girl and pet"
+          width={775}
+          height={900}
+        />
+      </Paper>
     </div>
   );
 }
--- src/components/Paper/Paper.jsx
+++ src/components/Paper/Paper.jsx
@@ -1,7 +1,9 @@
 import styles from "./Paper.module.css";
 
-export default function Paper({ color = "default" }) {
+export default function Paper({ color = "default", children }) {
   const containerClass = color === "default" ? "" : styles.customBackground;
 
-  return <div className={`${containerClass} ${styles.container}`}></div>;
+  return (
+    <div className={`${containerClass} ${styles.container}`}>{children}</div>
+  );
 }
