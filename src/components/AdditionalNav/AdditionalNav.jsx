90b7f61223d093b5f8287503e749a8807d8d7a97
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-08-28 03:36:40 +0300

    feat(AddNav): styled and updated component

--- src/components/AdditionalNav/AdditionalNav.jsx
+++ src/components/AdditionalNav/AdditionalNav.jsx
@@ -1,11 +1,43 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from "./AdditionalNav.module.css";
+import Like from "public/images/svg/like.svg";
+import Fav from "public/images/svg/fav.svg";
+import Dislike from "public/images/svg/dislike.svg";
 
 export default function AdditionalNav() {
+  const pathname = usePathname();
+
   return (
-    <div>
-      <Link href="/likes">Likes</Link>
-      <Link href="/favourites">Fav</Link>
-      <Link href="/dislikes">Dislikes</Link>
-    </div>
+    <ul className={styles.list}>
+      <li
+        className={`${styles.item} ${
+          pathname === "/likes" ? `${styles.active}` : ""
+        }`}
+      >
+        <Link className={styles.link} href="/likes">
+          <Like className={styles.icon} />
+        </Link>
+      </li>
+      <li
+        className={`${styles.item} ${
+          pathname === "/favourites" ? `${styles.active}` : ""
+        }`}
+      >
+        <Link className={styles.link} href="/favourites">
+          <Fav className={styles.icon} />
+        </Link>
+      </li>
+      <li
+        className={`${styles.item} ${
+          pathname === "/dislikes" ? `${styles.active}` : ""
+        }`}
+      >
+        <Link className={styles.link} href="/dislikes">
+          <Dislike className={styles.icon} />
+        </Link>
+      </li>
+    </ul>
   );
 }
