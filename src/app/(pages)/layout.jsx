6861b4e7ee069d4e0892eebfa18d76202093d1cc
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-08-28 03:58:16 +0300

    feat(): styled and updating inner layout

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -10,16 +10,27 @@ import AdditionalNav from "@/components/AdditionalNav/AdditionalNav";
 import BackBtn from "@/components/BackBtn/BackBtn";
 import SearchForm from "@/components/SearchForm/SearchForm";
 
+import styles from "./layout.module.css";
+import Paper from "@/components/Paper/Paper";
+
 export default function PagesLayout({ children }) {
   const pathname = usePathname();
 
   return (
     <section>
-      <SearchForm />
-      <AdditionalNav />
-      <BackBtn />
-      <h2>{pathname.replace("/", "").toUpperCase()}</h2>
-      {children}
+      <div className={styles.navWrapper}>
+        <SearchForm />
+        <AdditionalNav />
+      </div>
+      <Paper>
+        <div className={styles.innerWrapper}>
+          <BackBtn />
+          <h2 className={styles.title}>
+            {pathname.replace("/", "").toUpperCase()}
+          </h2>
+        </div>
+        {children}
+      </Paper>
     </section>
   );
 }
