bd102024549e6886b2d868bd6828d47b41e5a4ea
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-09-21 20:59:16 +0300

    feat(): add NoItemsFound comp if not search results

--- src/app/(pages)/search/page.jsx
+++ src/app/(pages)/search/page.jsx
@@ -4,16 +4,27 @@ import GridPattern from "@/components/GridPattern/GridPattern";
 import { useSearch } from "@/contexts/searchContext";
 
 import styles from "./page.module.css";
+import NoItemsFound from "@/components/NoItemsFound/NoItemsFound";
 
 export default function Search() {
   const { query, cats } = useSearch();
 
+  if (!cats) {
+    return null;
+  }
+
   return (
-    <div>
-      <p className={styles.p}>
-        Search results for: <span className={styles.span}>{query}</span>
-      </p>
-      <GridPattern cats={cats} />
-    </div>
+    <>
+      {cats.length === 0 ? (
+        <NoItemsFound />
+      ) : (
+        <div>
+          <p className={styles.p}>
+            Search results for: <span className={styles.span}>{query}</span>
+          </p>
+          <GridPattern cats={cats} />
+        </div>
+      )}
+    </>
   );
 }
