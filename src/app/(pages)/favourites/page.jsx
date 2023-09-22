072dd6b61f53037ab3fc87504c4abd9bf0fef7c9
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-09-22 16:46:16 +0300

    feat(): add Loader on Favs page

--- src/app/(pages)/favourites/page.jsx
+++ src/app/(pages)/favourites/page.jsx
@@ -1,36 +1,53 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFavourites from "@/hooks/useFavourites";
 import useLogs from "@/hooks/useLogs";
 import { getFavourites } from "@/utils/api";
 
 import LogsStory from "@/components/LogsStory/LogsStory";
 import GridPattern from "@/components/GridPattern/GridPattern";
+import Loader from "@/components/Loader/Loader";
+import NoItemsFound from "@/components/NoItemsFound/NoItemsFound";
 
 export default function Favourites() {
   const { favouritesCats, deleteFromFavourites, setFavouritesCats } =
     useFavourites();
   const { logs, addLog } = useLogs();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
+    setIsLoading(true);
     async function fetchData() {
       const data = await getFavourites();
 
       setFavouritesCats(data);
+      setIsLoading(false);
     }
 
     fetchData();
   }, [setFavouritesCats]);
 
   return (
-    <div>
-      <GridPattern
-        cats={favouritesCats}
-        deleteFromFavourites={deleteFromFavourites}
-        addLog={addLog}
-      />
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {!favouritesCats ? (
+            <NoItemsFound />
+          ) : (
+            <GridPattern
+              cats={favouritesCats}
+              deleteFromFavourites={deleteFromFavourites}
+              addLog={addLog}
+            />
+          )}
+        </>
+      )}
+
       <LogsStory logs={logs} />
-    </div>
+    </>
   );
 }
